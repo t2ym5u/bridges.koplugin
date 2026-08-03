b28d53fde3b6b373db613958d3630e5cf405d579
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-03 15:50:09 +0200

    chore: bump version to 1.1.11

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Bridges"),
     description = _("Connect islands with the right number of bridges"),
-    version     = "1.1.10",
+    version     = "1.1.11",
 }
