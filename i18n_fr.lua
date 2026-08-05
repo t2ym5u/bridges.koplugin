1e6861d5453bfeccf696a07dd68564651e9c73d6
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:30 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,9 +1,9 @@
 return {
-    ["Island selected: needs %1 bridges"]  = { fr = "Île sélectionnée : %1 pont(s) requis" },
-    ["Islands satisfied: %1/%2"]           = { fr = "Îles satisfaites : %1/%2" },
-    ["%1\xC3\x97%2 \xC2\xB7 %3 \xC2\xB7 %4/%5 islands done"] = { fr = "%1×%2 · %3 · %4/%5 îles terminées" },
-    ["Reset"]       = { fr = "Réinitialiser" },
-    ["Solved! All islands connected!"]        = { fr = "Résolu ! Toutes les îles sont connectées !" },
-    ["Bridges"]                                = { fr = "Ponts" },
-    ["Connect islands with the right number of bridges"] = { fr = "Reliez les îles avec le bon nombre de ponts" },
+    ["Island selected: needs %1 bridges"]  = { fr = "Île sélectionnée : %1 pont(s) requis", es = "Isla seleccionada: necesita %1 puentes", de = "Insel ausgewählt: benötigt %1 Brücken" },
+    ["Islands satisfied: %1/%2"]           = { fr = "Îles satisfaites : %1/%2", es = "Islas satisfechas: %1/%2", de = "Inseln erfüllt: %1/%2" },
+    ["%1\xC3\x97%2 \xC2\xB7 %3 \xC2\xB7 %4/%5 islands done"] = { fr = "%1×%2 · %3 · %4/%5 îles terminées", es = "%1×%2 · %3 · %4/%5 islas completadas", de = "%1×%2 · %3 · %4/%5 Inseln fertig" },
+    ["Reset"]       = { fr = "Réinitialiser", es = "Reiniciar", de = "Zurücksetzen" },
+    ["Solved! All islands connected!"]        = { fr = "Résolu ! Toutes les îles sont connectées !", es = "¡Resuelto! ¡Todas las islas están conectadas!", de = "Gelöst! Alle Inseln verbunden!" },
+    ["Bridges"]                                = { fr = "Ponts", es = "Puentes", de = "Brücken" },
+    ["Connect islands with the right number of bridges"] = { fr = "Reliez les îles avec le bon nombre de ponts", es = "Conecta las islas con el número correcto de puentes", de = "Verbinde die Inseln mit der richtigen Anzahl Brücken" },
 }
